630c0ec1e528310d90aebf677a8bbdad8dc0cc38
Максим Сын Отца Своего <noimxl1@gmail.com>
2024-12-02 16:18:12 +0300

    02.12

--- assets/js/news-section-main-page.js
+++ assets/js/news-section-main-page.js
@@ -1,5 +1,7 @@
 let btns = document.querySelectorAll('.nav-btns a');
 let moreBtns = document.querySelectorAll('.more-btns .btn-more-box');
+let wrapperBox = document.querySelector('.wrapper-news');
+console.log(wrapperBox);
 let contentWrapper = document.querySelectorAll('.wrapper-news .elem');
 console.log(contentWrapper);
 btns.forEach((point, index) => {
@@ -13,6 +15,12 @@ btns.forEach((point, index) => {
         contentWrapper.forEach(content => {
             content.classList.remove('active');
         });
+        if(index === 1 || index === 2){
+            wrapperBox.classList.add('elm');
+        }
+        else{
+            wrapperBox.classList.remove('elm');
+        }
         point.classList.add('active');
         moreBtns[index].classList.add('active');
         contentWrapper[index].classList.add('active');
