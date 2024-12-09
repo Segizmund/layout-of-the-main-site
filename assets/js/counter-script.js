bd398583fd78a1e8d301d2b95e24dd942a3239ff
Максим Сын Отца Своего <noimxl1@gmail.com>
2024-12-09 14:43:55 +0300

    09.12

--- assets/js/counter-script.js
+++ assets/js/counter-script.js
@@ -10,10 +10,17 @@ function createCounter(elementId, targetValue, interval, step = 1) {
             clearInterval(intervalId);
         }
     }
+    const observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+            intervalId = setInterval(increment, interval);
+            observer.unobserve(counterElement); // Останавливаем наблюдение после запуска
+        }
+    });
 
-    intervalId = setInterval(increment, interval);
+    observer.observe(counterElement);
     return intervalId;
 }
+
 function createCounterDecay(elementId, startValue, interval, step = 1) {
     const counterElement = document.getElementById(elementId);
     let count = startValue;
@@ -27,12 +34,18 @@ function createCounterDecay(elementId, startValue, interval, step = 1) {
         }
     }
 
-    intervalId = setInterval(decrement, interval);
+    const observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+            intervalId = setInterval(decrement, interval);
+            observer.unobserve(counterElement); // Останавливаем наблюдение после запуска
+        }
+    });
+
+    observer.observe(counterElement);
     return intervalId;
 }
-
-const intervalIdFaculty = createCounter('countFaculty', 7, 100,1);
-const intervalIdKaf = createCounter('countKaf', 40, 10,1);
-const intervalIdStud = createCounter('countStud', 15500, 1,100);
-const intervalIdObl = createCounterDecay('countObl', 100, 1,1);
-const intervalIdProg = createCounter('countProg', 96, 1,1);
+    const intervalIdFaculty = createCounter('countFaculty', 7, 100, 1);
+    const intervalIdKaf = createCounter('countKaf', 40, 10, 1);
+    const intervalIdStud = createCounter('countStud', 15500, 1, 100);
+    const intervalIdObl = createCounterDecay('countObl', 100, 1, 1);
+    const intervalIdProg = createCounter('countProg', 96, 1, 1);
