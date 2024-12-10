bd550a3d0f8cd2ecff6306ccc2f7e9ae1978271a
Максим Сын Отца Своего <noimxl1@gmail.com>
2024-12-10 14:08:45 +0300

    10.12

--- assets/js/counter-script.js
+++ assets/js/counter-script.js
@@ -44,8 +44,20 @@ function createCounterDecay(elementId, startValue, interval, step = 1) {
     observer.observe(counterElement);
     return intervalId;
 }
-    const intervalIdFaculty = createCounter('countFaculty', 7, 100, 1);
-    const intervalIdKaf = createCounter('countKaf', 40, 10, 1);
-    const intervalIdStud = createCounter('countStud', 15500, 1, 100);
-    const intervalIdObl = createCounterDecay('countObl', 100, 1, 1);
-    const intervalIdProg = createCounter('countProg', 96, 1, 1);
+if(document.querySelector('.box-number')) {
+    if(document.querySelector('#countFaculty')) {
+        const intervalIdFaculty = createCounter('countFaculty', 7, 100, 1);
+    }
+    if(document.querySelector('#countKaf')) {
+        const intervalIdKaf = createCounter('countKaf', 40, 10, 1);
+    }
+    if(document.querySelector('#countStud')) {
+        const intervalIdStud = createCounter('countStud', 15500, 1, 100);
+    }
+    if(document.querySelector('#countObl')) {
+        const intervalIdObl = createCounterDecay('countObl', 100, 1, 1);
+    }
+    if(document.querySelector('#countProg')) {
+        const intervalIdProg = createCounter('countProg', 96, 1, 1);
+    }
+}
