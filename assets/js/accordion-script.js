f3c7ed023d684b000e9a8ebf98ec3863ad5a5c83
Максим Сын Отца Своего <noimxl1@gmail.com>
2024-12-25 16:43:58 +0300

    25.12

--- assets/js/accordion-script.js
+++ assets/js/accordion-script.js
@@ -6,33 +6,37 @@ document.addEventListener('DOMContentLoaded', function() {
         const accordionBox = document.querySelectorAll('.accordion-box');
         const accordionArrow = document.querySelectorAll('.accordion-box svg');
         var height = [];
-
+        var defaultHeight = [];
+        var rect = [];
+        var newWidth = [];
         accordionItems.forEach((item, index) => {
-            var rect = accordionText[index].getBoundingClientRect();
-            height[index] = rect.height;
-            height[index] += 75;
-            var newWidth = accordionItems[index].offsetWidth;
-            accordionContentBox[index].style.width = newWidth + 'px';
+            rect[index] = accordionText[index].getBoundingClientRect();
+            newWidth[index] = accordionItems[index].offsetWidth;
+            accordionContentBox[index].style.width = newWidth[index] + 'px';
+            defaultHeight[index] = accordionBox[index].getBoundingClientRect().height;
+
+            height[index] = rect[index].height;
+            height[index] += parseInt(defaultHeight[index]);
 
             window.addEventListener('resize', () => {
-                newWidth = accordionItems[0].offsetWidth;
-                accordionContentBox[index].style.width = newWidth + 'px';
-                rect = accordionText[index].getBoundingClientRect();
+                newWidth[index] = accordionItems[0].offsetWidth;
+                accordionContentBox[index].style.width = newWidth[index] + 'px';
+                rect[index] = accordionText[index].getBoundingClientRect();
                 height[index] = 0;
-                height[index] = rect.height + 75;
+                height[index] = rect[index].height + 75;
             });
             item.addEventListener('click', function () {
                 const content = accordionContentBox[index];
                 const isExpanded = content.classList.contains('show');
-
                 content.classList.toggle('show', !isExpanded);
                 if (content.classList.contains('show')) {
                     accordionBox[index].classList.add('show');
                     accordionBox[index].style.height = height[index] + 'px';
+                    console.log(height[index]);
                     accordionArrow[index].style.transform = 'rotateZ(180deg)';
                 } else {
                     accordionBox[index].classList.remove('show');
-                    accordionBox[index].style.height = '73px';
+                    accordionBox[index].style.height = defaultHeight[index] + 'px';
                     accordionArrow[index].style.transform = 'rotateZ(0deg)';
                 }
             });
