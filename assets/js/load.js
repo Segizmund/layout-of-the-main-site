54da099b9f5911481c55ef3febad5304167e1448
Максим Сын Отца Своего <noimxl1@gmail.com>
2025-01-16 11:00:52 +0300

    16.01

--- assets/js/load.js
+++ assets/js/load.js
@@ -4,18 +4,24 @@ function checkScreenWidth() {
     const screenWidth = window.innerWidth;
 
     if (screenWidth >= 1140) {
-        video.src = '../assets/video/2KK.webm';
+        video.addEventListener('play', function () {
+            document.body.style.overflow = 'hidden';
+        });
+        video.addEventListener('ended', () => {
+            video.classList.add('end');
+            document.body.style.overflow = 'auto';
+        }, {once: true})
     } else {
         video.src = '../assets/video/load-mob.webm';
+        video.addEventListener('play', function () {
+            document.body.style.overflow = 'hidden';
+        });
+        video.addEventListener('ended', () => {
+            video.classList.add('end');
+            document.body.style.overflow = 'auto';
+        }, {once: true})
 
     }
-    video.addEventListener('play', function () {
-        document.body.style.overflow = 'hidden';
-    });
-    video.addEventListener('ended', () => {
-        video.classList.add('end');
-        document.body.style.overflow = 'auto';
-    }, {once: true})
 }
 
 window.onload = checkScreenWidth;
