3ceea90b7d11df001c7b0c0ea16d4b83de841714
Максим Сын Отца Своего <noimxl1@gmail.com>
2025-01-21 10:59:28 +0300

    21.01

--- assets/js/Modal-on-main-page.js
+++ assets/js/Modal-on-main-page.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     if (document.querySelector('.excursion-btn')) {
         let btnModal = document.querySelector('.excursion-btn');
-        let Modal = document.querySelector('.excursion-menu');
+        let modalBox = document.querySelector('.excursion-menu-box');
+        let modal = document.querySelector('.excursion-menu');
 
         btnModal.addEventListener('click', function () {
-            Modal.classList.toggle('open')
+            modal.classList.toggle('open')
+            modalBox.classList.toggle('open')
         });
 
     }
