44ccd1db7eb949a90ef1feaef17ec06b249106f8
Максим Сын Отца Своего <noimxl1@gmail.com>
2025-01-30 14:30:52 +0300

    30.01

--- assets/js/aware-button-animaton.js
+++ assets/js/aware-button-animaton.js
@@ -1,45 +1,47 @@
-const positionAwareElements = document.querySelectorAll('.position-aware');
-positionAwareElements.forEach(element => {
-    let isAnimating = false;
-    let animationFrame;
+if(document.querySelector('.position-aware')) {
+    const positionAwareElements = document.querySelectorAll('.position-aware');
+    positionAwareElements.forEach(element => {
+        let isAnimating = false;
+        let animationFrame;
 
-    element.addEventListener('mouseenter', (event) => {
-        const elementRect = element.getBoundingClientRect();
-        const relativeX = event.clientX - elementRect.left;
-        const relativeY = event.clientY - elementRect.top;
-        const span = element.querySelector('.aware-bg');
+        element.addEventListener('mouseenter', (event) => {
+            const elementRect = element.getBoundingClientRect();
+            const relativeX = event.clientX - elementRect.left;
+            const relativeY = event.clientY - elementRect.top;
+            const span = element.querySelector('.aware-bg');
 
-        let currentWidth = 2.5;
-        const targetWidth = 100;
-        let currentHeight = 2.5;
-        const targetHeight = 100;
-        const increment = 10;
+            let currentWidth = 2.5;
+            const targetWidth = 100;
+            let currentHeight = 2.5;
+            const targetHeight = 100;
+            const increment = 10;
 
-        isAnimating = true;
+            isAnimating = true;
 
-        function animate() {
-            if (!isAnimating) return;
+            function animate() {
+                if (!isAnimating) return;
 
-            if (currentWidth < targetWidth || currentHeight < targetHeight) {
-                currentWidth += increment;
-                currentHeight += increment;
-                span.style.width = currentWidth + 'vw';
-                span.style.height = currentHeight + 'vw';
-                animationFrame = requestAnimationFrame(animate);
+                if (currentWidth < targetWidth || currentHeight < targetHeight) {
+                    currentWidth += increment;
+                    currentHeight += increment;
+                    span.style.width = currentWidth + 'vw';
+                    span.style.height = currentHeight + 'vw';
+                    animationFrame = requestAnimationFrame(animate);
+                }
             }
-        }
 
-        span.style.top = `${relativeY}px`;
-        span.style.left = `${relativeX}px`;
-        animate();
+            span.style.top = `${relativeY}px`;
+            span.style.left = `${relativeX}px`;
+            animate();
+        });
+
+        element.addEventListener('mouseleave', () => {
+            isAnimating = false;
+            cancelAnimationFrame(animationFrame);
+
+            const span = element.querySelector('.aware-bg');
+            span.style.width = '0';
+            span.style.height = '0';
+        });
     });
-
-    element.addEventListener('mouseleave', () => {
-        isAnimating = false;
-        cancelAnimationFrame(animationFrame);
-
-        const span = element.querySelector('.aware-bg');
-        span.style.width = '0';
-        span.style.height = '0';
-    });
-});
+}
